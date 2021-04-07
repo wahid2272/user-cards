cf8189a3fb36f00e485d81d54fef8475808ffae4
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-04-07 15:35:34 +0300

    Navbar component added and designed with material-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 
 function App() {
   return (
-    <div className="App">
-      Hello!
-    </div>
+    <>
+      <Router>
+        <>
+          <CssBaseline />
+          <div className="appBody">
+            <Navbar />
+          </div>
+        </>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,41 @@
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  button: {
+    textTransform: 'none',
+    fontSize: 'larger'
+  }
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Button color="inherit" component={Link} to={"/"} className={classes.button}>
+              Home
+          </Button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
